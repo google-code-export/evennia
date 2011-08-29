1d86edf6087a089662e5537504e5e9a921738c2e
griatch <griatch@localhost>
2011-08-29 21:31:12 +0000

    branch: More work on Windows support: handles cleanup of pidfiles better.

--- src/server/portal.py
+++ src/server/portal.py
@@ -14,6 +14,7 @@ if os.name == 'nt':
     # For Windows batchfile we need an extra path insertion here.
     sys.path.insert(0, os.path.dirname(os.path.dirname(
                 os.path.dirname(os.path.abspath(__file__)))))
+    PORTAL_PIDFILE = os.path.join(settings.GAME_DIR, 'portal.pid')
 
 from twisted.application import internet, service
 from twisted.internet import protocol, reactor
@@ -90,7 +91,7 @@ class Portal(object):
 
         # set a callback if the server is killed abruptly, 
         # by Ctrl-C, reboot etc.
-        #reactor.addSystemEventTrigger('before', 'shutdown', self.shutdown, _abrupt=True)
+        reactor.addSystemEventTrigger('before', 'shutdown', self.shutdown, _abrupt=True)
 
         self.game_running = False
                 
@@ -134,13 +135,15 @@ class Portal(object):
         f.write(str(mode))
         f.close()
 
-    def shutdown(self, restart=None):
+    def shutdown(self, restart=None, _abrupt=False):
         """
         Shuts down the server from inside it. 
 
         restart - True/False sets the flags so the server will be
                   restarted or not. If None, the current flag setting
                   (set at initialization or previous runs) is used.
+        _abrupt - this is set if server is stopped by a kill command,
+                  in which case the reactor is dead anyway. 
 
         Note that restarting (regardless of the setting) will not work
         if the Portal is currently running in daemon mode. In that
@@ -149,6 +152,9 @@ class Portal(object):
         print "Portal shutdown:", restart
         self.set_restart_mode(restart)
         reactor.callLater(0, reactor.stop)
+        if os.name == 'nt' and os.path.exists(PORTAL_PIDFILE):
+            # for Windows we need to remove pid files manually            
+            os.remove(PORTAL_PIDFILE)
 
 #------------------------------------------------------------
 #
